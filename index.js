66041bb9568e486f2374922141abd06c14c77ccf
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2022-07-07 12:17:42 -0400

    Complete

---
+++ index.js
@@ -0,0 +1,29 @@
+// const $bigBall = document.querySelector('.cursor__ball--big');
+// const $hoverables = document.querySelectorAll('.hoverable');
+
+// // Listeners
+// document.body.addEventListener('mousemove', onMouseMove);
+// for (let i = 0; i < $hoverables.length; i++) {
+//   $hoverables[i].addEventListener('mouseenter');
+//   $hoverables[i].addEventListener('mouseleave');
+// }
+
+// // Move the cursor
+// function onMouseMove(e) {
+//   TweenMax.to($bigBall, .4, {
+//     x: e.pageX - 15,
+//     y: e.pageY - 15
+//   })
+// }
+
+// // Hover an element
+// function onMouseHover() {
+//   TweenMax.to($bigBall, .3, {
+//     scale: 4
+//   })
+// }
+// function onMouseHoverOut() {
+//   TweenMax.to($bigBall, .3, {
+//     scale: 1
+//   })
+// }
